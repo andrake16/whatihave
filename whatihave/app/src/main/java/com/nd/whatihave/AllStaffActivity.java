e9df04b986ae197461c329c5a34533bc36740372
andrake16 <andrake16@gmail.com>
2017-01-18 01:03:52 +0700

    first commit

---
+++ whatihave/app/src/main/java/com/nd/whatihave/AllStaffActivity.java
@@ -0,0 +1,13 @@
+package com.nd.whatihave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_staff);
+    }
+}
